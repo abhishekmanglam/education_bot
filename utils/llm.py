1c663934fda783b83c3159a2415c9cf186f6cb49
abhishekmanglam <106091056+abhishekmanglam@users.noreply.github.com>
2026-06-26 17:47:25 +0530

    Modify temperature and comment out secret key checks
    Updated temperature setting and commented out secret key handling.

--- utils/llm.py
+++ utils/llm.py
@@ -1,18 +1,20 @@
 import streamlit as st
+from langchain_groq import ChatGroq
+
 @st.cache_resource
 def load_llm():
 
-    st.write("Secrets keys:", list(st.secrets.keys()))
+    # st.write("Secrets keys:", list(st.secrets.keys()))
 
-    if "GROQ_API_KEY" in st.secrets:
-        key = st.secrets["GROQ_API_KEY"]
-        st.write("Key prefix:", key[:10])
-    else:
-        st.error("GROQ_API_KEY NOT FOUND")
-        st.stop()
+    # if "GROQ_API_KEY" in st.secrets:
+    #     key = st.secrets["GROQ_API_KEY"]
+    #     st.write("Key prefix:", key[:10])
+    # else:
+    #     st.error("GROQ_API_KEY NOT FOUND")
+    #     st.stop()
 
     return ChatGroq(
         model="llama-3.3-70b-versatile",
-        temperature=0.5,
+        temperature=0.4,
         api_key=key
     )
